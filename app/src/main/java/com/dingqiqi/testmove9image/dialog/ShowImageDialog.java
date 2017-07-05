c25bdbba6de55120b0c59a4e1e64dd3d91fa4d83
dingqiqi <dingqiqi8@qq.com>
2017-07-05 17:16:23 +0800

    提交代码

---
+++ app/src/main/java/com/dingqiqi/testmove9image/dialog/ShowImageDialog.java
@@ -0,0 +1,51 @@
+package com.dingqiqi.testmove9image.dialog;
+
+import android.app.DialogFragment;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.dingqiqi.testmove9image.R;
+
+public class ShowImageDialog extends DialogFragment {
+
+    private ImageView mIvShow;
+
+    private Bitmap mBitmap;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NORMAL, R.style.Dialog_FullScreen);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_show_image_layout, container,
+                false);
+
+        mIvShow = (ImageView) view.findViewById(R.id.iv_show);
+        mIvShow.setImageBitmap(mBitmap);
+
+        return view;
+    }
+
+    /**
+     * 设置监听
+     *
+     * @param bitmap 图片
+     */
+    @SuppressWarnings("deprecation")
+    public void setBackground(Bitmap bitmap) {
+        mBitmap = bitmap;
+        if (mIvShow != null) {
+            mIvShow.setImageBitmap(mBitmap);
+        }
+    }
+}
